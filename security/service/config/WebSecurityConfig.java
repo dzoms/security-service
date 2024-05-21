660b402a26fe10e90ea23ae636690b9569f3666b
Vyach12 <slava.gusarov.73@mail.ru>
2024-05-21 11:42:51 +0300

    [feat]: added contact api for creating and getting contacts

--- security/service/config/WebSecurityConfig.java
+++ security/service/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,6 +17,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         //.requestMatchers(HttpMethod.GET, "/api/v1/user-contact").permitAll()
                         //.requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
